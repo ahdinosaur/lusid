9001ed7d3202f481516b4ad3aaee80aa0559e70f
Michael Williams <hello@mikey.nz>
2026-02-02 16:51:26 +1300

    Add system

--- system/src/lib.rs
+++ system/src/lib.rs
@@ -2,8 +2,17 @@ mod arch;
 mod hardware;
 mod id;
 mod os;
+mod user;
 
 pub use crate::arch::*;
 pub use crate::hardware::*;
 pub use crate::id::*;
 pub use crate::os::*;
+pub use crate::user::*;
+
+pub struct System {
+    pub hostname: Hostname,
+    pub arch: Arch,
+    pub os: Os,
+    pub user: User,
+}
